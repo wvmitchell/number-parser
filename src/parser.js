7b5d247b9a73c9016ceaa4804259cc3b58a0d55c
Kayla Wood <kaylaewood@gmail.com>
2023-01-18 17:06:08 -0700

    Add comments to clarify code

--- src/parser.js
+++ src/parser.js
@@ -44,37 +44,52 @@ const parser = (numString) => {
     let currString = words[i];
     let currNum = numbers[currString];
 
+    // base case
     if(!currString) {
       return;
     }
 
+    // deal with hyphen
     if(currString.includes('-')) {
       currNum = unhyphenate(currString);
     }
 
+    // if it's the last word, add to sum
     if(!words[i + 1]) {
       sum += currNum;
+
+    // if 'and,' keep moving
     } else if(currString === 'and') {
       updateSum(i + 1);
+
+    // if the next two words are multipliers ('hundred thousand'), perform math
     } else if(multipliers[words[i + 1]] && multipliers[words[i + 2]]) {
       sum += currNum * multipliers[words[i + 1]] * multipliers[words[i + 2]];
       updateSum(i + 3);
+
+    // if the next single word is a multiplier...
     } else if(multipliers[words[i + 1]]) {
+
+      // ...and there is 'thousand' written AFTER 'hundred', multiply current sum by 1,000 (would need to make this much more dynamic if millions came into play)
       if(words[i + 1] === 'thousand' && numString.split('thousand')[0].includes('hundred')) {
         sum += currNum;
         sum *= 1000;
         return updateSum(i + 2);
       }
+
+      // ...otherwise, do the math for the single multiplier
       sum += currNum * multipliers[words[i + 1]];
       updateSum(i + 2);
     }
   }
 
+  // start recursion
   updateSum(0);
 
   return sum;
 }
 
+// helpers:
 const unhyphenate = (string) => {
   const nums = string.split('-');
 
